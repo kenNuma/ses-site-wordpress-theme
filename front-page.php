2483e9b265da79aa9f071f06bb3dc83593012099
嘉村 <kamurakenichirou@kamurakenichirounoMacBook-Pro.local>
2026-02-06 09:04:02 +0900

    create-FV

--- front-page.php
+++ front-page.php
@@ -1,10 +1,16 @@
 <?php
 get_header();
 ?>
-<!-- <main>
-    <h1>テーマ有効化テスト</h1>
-    <p>ここが表示されればOK。</p>
-    <p>front-page</p>
-</main> -->
+<main>
+    <section class="fv">
+        <div class="fv-inner">
+            <div class="catch-copy shadow">
+                <h1>技術と人を、最適につなぐ。</h1>
+                <p>高精度なスキルマッチと迅速な対応で、<br>現場にフィットするエンジニアを提供します。</p>
+            </div>
+            <a class="fv-cta cta-btn"href="">お問い合わせ</a>
+        </div>
+    </section>
+</main>
 <?php
 get_footer();
